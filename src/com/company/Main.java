f673e50e337a5391cb628545081bd1d287fd0977
Елена Денисова <E-denisova@yandex.ru>
2022-01-31 16:35:06 +0300

    INIAL COMMIT

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	    int nOriginalBalance = 2_000_000_000;
+        int nDepositAmount = 500_000_000;
+        int nFinalBalance = nOriginalBalance + nDepositAmount;
+
+        System.out.println(nFinalBalance);
+    }
+}
